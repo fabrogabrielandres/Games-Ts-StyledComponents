35014c63301eb30f76d9676ddf50a086d192b511
Gabriel Fabro <84206590+fabrogabrielandres@users.noreply.github.com>
2021-11-16 18:05:14 -0600

    footer

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,23 +1,44 @@
 import React, { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
-
 import { Game } from '../../types/types';
-import { Img, StyledLink } from './styles';
-
+import { Img, StyledLink, Title, Details, Description, Genre, Icon,Footer } from './styles';
+import { BROWSER, WINDOWS } from './constants';
+import windowsIcon from '../../../assets/icons/windows.svg';
+import browserIcon from '../../../assets/icons/browser.svg';
 interface Props {
   content: Game;
 }
 
 export const GameCard = ({ content }: Props): ReactElement => {
-  const { id, title, thumbnail, short_description, genre } = content;
+  const { id, title, thumbnail, short_description, genre, platform } = content;
+  console.log(platform);
+
+  const icons = platform.split(',').map((p, idx) => {
+    let icon = null;
+    switch (p.trim()) {
+      case BROWSER:
+        icon = <Icon key={idx} alt="" src={browserIcon} />;
+        break;
+      case WINDOWS:
+        icon = <Icon key={idx} alt="" src={windowsIcon} />;
+        break;
+      default:
+        break;
+    }
+    return icon;
+  });
 
   const link = `/game/${id}`;
   return (
     <StyledLink to={link}>
       <Img src={thumbnail} alt={`${title} logo`} />
-      <h2>{title}</h2>
-      <p>{short_description}</p>
-      <p>{genre}</p>
+      <Details>
+        <Title>{title}</Title>
+        <Description>{short_description}</Description>
+        <Footer>
+          {icons}
+          <Genre>{genre}</Genre>
+        </Footer>
+      </Details>
     </StyledLink>
   );
 };
--- src/components/GameCard/styles.tsx
+++ src/components/GameCard/styles.tsx
@@ -1,15 +1,62 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { secondaryColor } from "../../styles/theme";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import {
+  backgroundColor,
+  primaryColor,
+  primaryTextColor,
+  secondaryColor,
+  secondaryTextColor,
+  tertiaryTextColor,
+} from '../../styles/theme';
 
 export const StyledLink = styled(Link)`
-    display: inline-block;
-    width: 100%;
-    background-color: ${secondaryColor};
-    border-radius: 4px;
-    text-decoration: none; 
-`
+  display: inline-block;
+  width: 100%;
+  background-color: ${secondaryColor};
+  border-radius: 4px;
+  text-decoration: none;
+`;
 
 export const Img = styled.img`
-    width: 100%;
-`
+  width: 100%;
+`;
+export const Title = styled.h2`
+  margin: 0;
+  font-size: 24px;
+  font-weight: 510;
+  color: ${primaryTextColor};
+`;
+
+export const Details = styled.div`
+  padding: 20px;
+`;
+
+export const Description = styled.p`
+  font-size: 16px;
+  color: ${secondaryTextColor};
+`;
+
+export const Footer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+export const Genre = styled.p`
+  padding: 2px 4px;
+  margin: 0 8px 0 0;
+  border-radius: 4px;
+  font-size: 12px;
+  font-weight: bold;
+  color: ${tertiaryTextColor};
+  background-color: ${backgroundColor};
+  position: absolute;
+  right: 0;
+`;
+
+export const Icon = styled.img`
+  width: 20%;
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+`;
