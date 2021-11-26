3a08263b4612ceaa41010899aec42fc82078aa59
Gabriel Fabro <fabrogabrielandres@gmail.com>
2021-11-26 16:48:35 -0600

    finish

--- src/components/GameFilter/index.tsx
+++ src/components/GameFilter/index.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, ReactElement } from 'react';
 import { GENRES, PLATFORMS, SORT_BY, TAGS } from './constants';
+import { Form, Label, Select } from './styles';
 
 interface Props {
   onChange: (e: ChangeEvent<HTMLFormElement>) => void;
@@ -7,47 +8,47 @@ interface Props {
 
 export const GameFilter = ({ onChange }: Props): ReactElement => {
   return (
-    <form onChange={onChange}>
-      <label htmlFor="platform-select">
+    <Form onChange={onChange}>
+      <Label htmlFor="platform-select">
         Platform:
-        <select name="platform" id="platform-select">
+        <Select name="platform" id="platform-select">
           {PLATFORMS.map((platform) => (
             <option key={platform.value} value={platform.value}>
               {platform.display}
             </option>
           ))}
-        </select>
-      </label>
-      <label htmlFor="genre-select">
+        </Select>
+      </Label>
+      <Label htmlFor="genre-select">
         GENRES:
-        <select name="genre" id="genre-select">
+        <Select name="genre" id="genre-select">
           {GENRES.map((genre) => (
             <option key={genre.value} value={genre.value}>
               {genre.display}
             </option>
           ))}
-        </select>
-      </label>
-      <label htmlFor="tag-select">
+        </Select>
+      </Label>
+      <Label htmlFor="tag-select">
         TAGS:
-        <select name="tag" id="tag-select">
+        <Select name="tag" id="tag-select">
           {TAGS.map((tag) => (
             <option key={tag.value} value={tag.value}>
               {tag.display}
             </option>
           ))}
-        </select>
-      </label>
-      <label htmlFor="sortby-select">
+        </Select>
+      </Label>
+      <Label htmlFor="sortby-select">
         SORT_BY:
-        <select name="sortby" id="sortby-select">
+        <Select name="sortby" id="sortby-select">
           {SORT_BY.map((sortby) => (
             <option key={sortby.value} value={sortby.value}>
               {sortby.display}
             </option>
           ))}
-        </select>
-      </label>
-    </form>
+        </Select>
+      </Label>
+    </Form>
   );
 };
